6a07bb1614a7021695bd34e0efefe08993e14173
Patrick M2 <patricks@patrick.com>
2023-09-17 14:42:14 -0700

    section14 added GroceryCategoryResponseDTO and GroceryCategoryRequestDTO

---
+++ Sources/GroceryAppSharedDTO/GroceryCategoryRequestDTO.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Dodge Bobson on 9/17/23.
+//
+
+import Foundation
+
+public struct GroceryCategoryRequestDTO: Codable {
+    public let title: String
+    public let colorCode: String
+    
+    public init(title: String, colorCode: String) {
+        self.title = title
+        self.colorCode = colorCode
+    }
+}
